b7a999c04c88a241658de60dd9db7f859d99b903
1plus1equal3 <115053943+1plus1equal3@users.noreply.github.com>
2023-06-09 20:14:02 +0700

    Initial

--- OOP_week1/src/FirstDialog.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-//Example 2: FirstDialog.java
-public class FirstDialog {
-    public static void main(String[] args) {
-        JOptionPane.showMessageDialog(null, "Hello world! How are you?");
-        System.exit(0);
-    }
-}
